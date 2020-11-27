7393aa6d46b677117833c406d42d863e310c520d
Felix <pointer_v@qq.com>
2020-11-27 14:58:28 +0800

    chore(@hecate/example): 删除Example工程中的代码，将其迁移为模块
    affects: @hecate/example

--- packages/@hecate/example/src/lib/modeler/configurations/index.js
+++
@@ -1,5 +0,0 @@
-import { leftPanelComponents } from './Components';
-import constants from './Constants';
-import definitions from './Schemas';
-
-export { leftPanelComponents, constants, definitions };
